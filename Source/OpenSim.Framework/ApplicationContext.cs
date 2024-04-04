daab607dd0902adb9b07f058b087b968860a3b04
Michael Dickson <mike.dickson@utopiaskye.com>
2024-04-04 11:05:39 -0400

    Create a static singleton to contain a component resolver and logger instance.   It needs to be initialized during application startup.   ServiceLocation is an anti-pattern in the DI based model because ideally a classes configuration should be injected not looked up.   But we gotta walk before we can run and this should help.

---
+++ Source/OpenSim.Framework/ApplicationContext.cs
@@ -0,0 +1,63 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OpenSim.Framework
+{
+    public sealed class ApplicationContext
+    {
+        private static ApplicationContext _instance;
+        private static readonly object _lock = new object();
+
+        private IComponentContext? _componentContext = null;
+        private ILoggerFactory? _loggerFactory = null;
+        private IConfiguration? _configuration = null;
+
+        public IComponentContext? ComponentContext { get; private set; }
+
+        public IConfiguration? Configuration { get; private set; }
+
+        public ILoggerFactory? LoggerFactory { get; private set; }
+        public ILogger? CreateLogger<T>() => LoggerFactory?.CreateLogger<T>();
+        public ILogger? CreateLogger(string categoryName) => LoggerFactory?.CreateLogger(categoryName);
+
+        // Private constructor to prevent instantiation from outside
+        private ApplicationContext()
+        {
+        }
+
+        public static void Initialize(IComponentContext? componentContext)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new ApplicationContext();
+                }
+
+                _instance._componentContext = componentContext;
+
+                _instance._loggerFactory = componentContext?.Resolve<ILoggerFactory>();
+                _instance._configuration = componentContext?.Resolve<IConfiguration>();
+            }
+        }
+
+        // Public static method to get the instance
+        public static ApplicationContext GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ApplicationContext();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+}
+
